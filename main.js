51295d395ed07e7b4247d99f851801f3e54d1ea6
S-Queen <sake4951@gmail.com>
2021-10-22 18:24:45 +0900

    完成

--- main.js
+++ main.js
@@ -4,17 +4,6 @@ const Age = document.getElementById("Age");
 const University = document.getElementById("University");
 const Hobbby = document.getElementById("Hobby");
 
-//Nameをクリックしたら、下に名前を表示させたい
-//Name.onclick = function () {
-//Nameを押したときのイベント
-//const nameText = document.createElement("div"); //nameMineというdivを作る
-//nameText.className = "nameText"; //nameMineというクラス
-//nameText.textContent = "Saki Takeda"; //Saki Takedaと表示させる。
-//};
-
-//jsからhtmlに表示されない！！
-//myAnswer.append(nameText);
-
 //好きな映画Quiz
 //アベンジャーズ、グッドウィルハンティング、ガタカのうち私が一番好きな映画は何でしょう？
 
@@ -24,6 +13,12 @@ const gattaca = document.getElementById("Gattaca");
 const answer = document.getElementById("answer");
 const movieContents = document.getElementById("movieContensts");
 
+//music カーソル当てたらテキストと好きなアーティスト表示？
+//Bowie, Queen, Harry あたり
+
+//drawing カーソル当てたらテキストとIGのページに移動？させたい
+
+//quiz
 avengers.onclick = function () {
   answer.textContent = "Close!";
 };
@@ -33,4 +28,4 @@ goodWill.onclick = function () {
 gattaca.onclick = function () {
   answer.textContent = "Close!";
 };
-//映画の詳細をanswerの下に表示したいな
+//カーソル当てたら映画の詳細をanswerの下に表示？
